043eb4f0d7e0dd25547482a87df61ba574140af9
StephanyValarezo <valarezo@fiec.espol.edu.ec>
2019-01-30 15:48:40 -0500

    Ventanas de log in, registrar y perfil para editar

---
+++ resources/views/login.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>	
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="empresa de servicions tecnologicos">
+<title>Mirai Projects</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<link href="css/style3.css" rel="stylesheet">	
+</head>
+
+
+
+
+
+
+
+
+
+
+<body>
+
+
+<div class="wrapper">
+
+	<header>
+
+<nav class="navbar navbar-expand-md navbar-light"> 
+<div class="container-fluid">
+	<a class="navbar-brand"  href="inicio.html">
+		<img class="logo" src="imagenes/black 32.png">
+	</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="inicio2.html">Inicio</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="servicios.html">Servicios</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="portafolio.html">Portafolio</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="galeria.html">Galeria</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="contacto.html">Contacto</a>
+			</li>
+			
+		</ul>
+</div>
+</nav>
+
+
+
+
+
+
+
+<div class="container">
+        <div class="card card-container">
+            
+
+            <img id="profile-img" class="profile-img-card" src="./imagenes/bgcolor.jpg">
+
+            <p id="profile-name" class="profile-name-card"></p>
+
+            <form class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div id="remember" class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Recuérdame
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Accede</button>
+            </form><!-- /form -->
+            <p> ¿Eres nuevo?<a href="#" class="forgot-password">
+                Regístrate
+            </a></p>
+        </div><!-- /card-container -->
+    </div><!-- /container -->
+
+
+
+</header>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+
+
+
+
+
+</html>
---
+++ resources/views/registrar.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>	
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="empresa de servicions tecnologicos">
+<title>Mirai Projects</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<link href="css/style3.css" rel="stylesheet">	
+</head>
+
+
+
+
+
+
+
+
+
+
+<body>
+
+
+<div class="wrapper">
+
+	<header>
+
+<nav class="navbar navbar-expand-md navbar-light"> 
+<div class="container-fluid">
+	<a class="navbar-brand"  href="inicio.html">
+		<img class="logo" src="imagenes/black 32.png">
+	</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="inicio2.html">Inicio</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="servicios.html">Servicios</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="portafolio.html">Portafolio</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="galeria.html">Galeria</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="contacto.html">Contacto</a>
+			</li>
+			
+		</ul>
+</div>
+</nav>
+
+
+
+
+
+
+
+<div class="container">
+        <div class="card card-container">
+            	
+        	<h2>Cuéntanos de tus ideas y nosotros lo haremos posible.</h2>
+            <form class="form-signin">
+            	
+                <div class="form-group">
+					<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+				</div>
+				<div class="form-group">
+					<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+				</div>
+
+				<div class="form-group">
+					<select class="form-control">
+					  <option>Cuéntanos a qué te dedicas</option>
+					  <option>Docente</option>
+					  <option>Estudiante</option>
+					  <option>Empresa pequeña (startups)</option>
+					  <option>Empresa grande (más de 100 empleados)</option>
+					  <option>Organización sin fines de lucro</option>
+					  <option>Uso personal</option>
+					</select>
+				</div>
+
+
+				<div class="form-group">
+					<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+				</div>
+				<div class="form-group">
+					<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+				</div>
+
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Registrar</button>
+            </form><!-- /form -->
+            
+        </div><!-- /card-container -->
+    </div><!-- /container -->
+
+
+
+</header>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+
+
+
+
+
+</html>
